3514590dc76a6de283b76c912adec764f0bde7f9
Aziz Al-Darrab <adarrab@gmail.com>
2020-06-03 18:24:27 +0300

    Add project files.

---
+++ AnimatingShadowFrameBug/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AnimatingShadowFrameBug
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnFrameClicked(object sender, EventArgs e)
+        {
+            var frame = (Frame)sender;
+            var bounds = frame.Bounds;
+            bounds.Y += 25;
+            await frame.LayoutTo(bounds, 500, Easing.CubicInOut);
+        }
+    }
+}
